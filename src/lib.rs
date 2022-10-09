6d9149a7ea840c38df750cc6f2cd4fbe37eca590
Thomas Versteeg <t@versteeg.email>
2022-10-09 18:13:30 +0200

    Improve performance

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
- #![crate_name = "distance_field"]
+#![crate_name = "distance_field"]
 
 extern crate image;
 extern crate spiral;
@@ -13,7 +13,7 @@ pub struct Options {
 
     /// The maximum distance for the projected point of the output image on the input image to
     /// search for the nearest point. The defaul value is: 512.
-    pub max_distance: u16,
+    pub max_distance: u32,
 
     /// The image value at which to apply the treshold. In a black-white vector image 127 is
     /// probably the best value. The default value is: 127.
@@ -31,7 +31,7 @@ impl Default for Options {
         Options {
             size: (64, 64),
             max_distance: 512,
-            image_treshold: 127
+            image_treshold: 127,
         }
     }
 }
@@ -53,40 +53,51 @@ impl DistanceFieldExt for DynamicImage {
     }
 }
 
-fn get_nearest_pixel_distance(input: &DynamicImage, out_x: u32, out_y: u32, options: &Options) -> u8 {
+fn get_nearest_pixel_distance(
+    input: &DynamicImage,
+    out_x: u32,
+    out_y: u32,
+    options: &Options,
+) -> u8 {
     let orig_size = input.dimensions();
 
     // Calcule the projected center of the output pixel on the source image
-    let center = ((out_x * orig_size.0) / options.size.0, (out_y * orig_size.1) / options.size.1);
+    let center = (
+        (out_x * orig_size.0) / options.size.0,
+        (out_y * orig_size.1) / options.size.1,
+    );
+    let center_signed = (center.0 as i32, center.1 as i32);
+    let orig_size = (orig_size.0 as i32, orig_size.1 as i32);
 
     // Check if we are inside a filled area so we can get the 127-255 range
     let is_inside = input.get_pixel(center.0, center.1).to_luma().0[0] > options.image_treshold;
 
-    let mut closest_distance = options.max_distance as f32;
-    for (x, y) in ManhattanIterator::new(center.0 as i32, center.1 as i32, options.max_distance as u16) {
-        if x < 0 || y < 0 || x >= orig_size.0 as i32 || y >= orig_size.1 as i32 {
-            continue;
-        }
+    let closest_distance = ManhattanIterator::new(
+        center_signed.0,
+        center_signed.1,
+        options.max_distance as i32,
+    )
+    // Ignore the boundary conditions
+    .filter(|(x, y)| *x >= 0 && *y >= 0 && *x < orig_size.0 && *y < orig_size.0)
+    // Continue if the center and this pixel are inside the filled area or
+    // the pixels are both outside the filled area
+    .find(|(x, y)| {
+        (input.get_pixel(*x as u32, *y as u32).to_luma().0[0] >= options.image_treshold)
+            != is_inside
+    })
+    // We found the nearest pixel, calculate the distance
+    .map(|(x, y)| {
+        let dx = (center_signed.0 - x).abs();
+        let dy = (center_signed.1 - y).abs();
 
-        let p = input.get_pixel(x as u32, y as u32).to_luma().0[0];
-        // Continue if the center and this pixel are inside the filled area or
-        // the pixels are both outside the filled area
-        if (p >= options.image_treshold) == is_inside {
-            continue;
-        }
-
-        // We found the nearest pixel, calculate the distance
-        let dx = (center.0 as i32 - x).abs();
-        let dy = (center.1 as i32 - y).abs();
-        closest_distance = ((dx * dx + dy * dy) as f32).sqrt();
-
-        break;
-    }
+        ((dx * dx + dy * dy) as f32).sqrt()
+    })
+    .unwrap_or(options.max_distance as f32);
 
     // Convert the outside to a 0.0-0.5 and the inside to a 0.5-1.0 range
     let distance_fraction = if is_inside {
         0.5 + (closest_distance / 2.0) / options.max_distance as f32
-    }else{
+    } else {
         0.5 - (closest_distance / 2.0) / options.max_distance as f32
     };
 
